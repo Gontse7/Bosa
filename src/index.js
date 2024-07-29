c6b7ec8a80e19843bdb5da9d58761e6ea327e4f3
Gontse7 <gontsemogana7@gmail.com>
2024-07-29 16:53:27 +0200

    added the weather forecast with real time data for the next 5 days

--- src/index.js
+++ src/index.js
@@ -22,6 +22,8 @@ weatherDescriptionElement.innerHTML = `${response.data.condition.description}`
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 speedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
+
+displayforecast(response.data.city);
     
 }
 
@@ -55,22 +57,42 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
-function displayForecast() {
- 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri", "Sat"];
 
-let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function displayforecast(city) {
+ let apiKey = "0t043164f467od1445504fadcb9bc012";
+ let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+ axios(apiURL).then(displayForecast);
+}
+
+function displayForecast(response) {
+ console.log(response.data);
+
 let forecastHtml = "";
-days.forEach(function (day) {
- forecastHtml = forecastHtml + `<div class="waether-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">⛅</div>
+response.data.daily.forEach(function (day, index) {
+  if (index < 5) {
+ forecastHtml =
+   forecastHtml +
+   `<div class="waether-forecast-day">
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  <div class="weather-forecast-icon"><img src ="${day.condition.icon_url}" class="weather-forecast-icon"/></div>
   <div class="weather-forecast-temps">
-    <div class="weather-forecast-temp"><strong>15°C</strong>
+    <div class="weather-forecast-temp"><strong>${Math.round(
+      day.temperature.maximum
+    )}°C</strong>
     </div>
-    <div class="weather-forecast-temp">9°</div>
+    <div class="weather-forecast-temp">${Math.round(
+      day.temperature.minimum
+    )}°C</div>
   </div>
 </div>
 `; 
+  }
 });
 let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHtml;
@@ -80,6 +102,5 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Gaborone");
-displayForecast();
 
     
